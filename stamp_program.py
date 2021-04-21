53036dbe0f77226c935a94f775337e651e1e89b6
nelli her <nellih1117@gmail.com>
2021-04-20 20:30:37 -0700

    fix rounding

--- stamp_program.py
+++ stamp_program.py
@@ -6,15 +6,21 @@ round answer to next number
 output the result to the master
 end
 """
+import math
+
+# input: sheet
 def calculate(sheet):
+  # step 1
   answer = sheet / 5
-  rounded = round(answer)
+  # step 2
+  rounded = math.ceil(answer)
   print("sheet is : ", sheet)
   print("the answer is :", answer)
   print("rounded is :", rounded)
   print("====================================")
+  # output: number of stamps needed 
   return rounded 
 
-output = calculate(10000)
+output = calculate(16)
 
-print("the return statement is:", output)
+print("the number of stamps needed is:", output)
